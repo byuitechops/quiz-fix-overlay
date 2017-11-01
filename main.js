301511a1bca004034c9deaeec5d88ea281b47183
Danverde <gre12014@byui.edu>
2017-11-01 16:56:38 -0600

    end of day commit. Almost working. Issue reading question.question_text

--- main.js
+++ main.js
@@ -1,24 +1,71 @@
 /*eslint-env node, es6*/
 
 /* Module Description */
+/* Replaces */
 
 /* Put dependencies here */
+const canvas = require('canvas-wrapper'),
+    asyncLib = require('async');
 
 
 module.exports = (course, stepCallback) => {
-    /* Create the module report so that we can access it later as needed.
-    This MUST be done at the beginning of each child module. */
-    course.addModuleReport('moduleName');
+    course.addModuleReport('quizFixOverlay');
 
-    /* Used to log successful actions */
-    course.success('moduleName', 'moduleName successfully ...');
+    function getQuestions(quiz, questionCb) {
 
-    /* How to report an error (Replace "moduleName") */
-    course.throwErr('moduleName', e);
-    /* You should never call the stepCallback with an error. We want the
-    whole program to run when testing so we can catch all existing errors */
+        function replaceHTML(question, replaceCb) {
+            var overlayHTML = `<div id="dialog_for_link_1" title="Title" class="enhanceable_content dialog">dialog</div><p><a id="${course.getOverlayCount}" class="Button" href="#dialog_for_link_1">prompt</a></p>`;
+            
 
-    stepCallback(null, course);
-    /* DO NOT call the callback on an error - instead, continue
-    with your child module's process */
+            if (question.question_text.includes('// <![CDATA[')) {
+                question.question_text.replace(/\/\/\s<!\[CDATA\[[\s\S]+\/\/\s\]\]>/, '');
+                question.question_text.replace(/<p>(?:\s+)?CHECK MY WORK(?:\s+)?<\/p>/i, '');
+                canvas.put(`/api/v1/courses/${course.info.canvasOU}/quizzes/${quiz.id}/questions/${question.id}`, {
+                    'quiz_id': quiz.id,
+                    'id': question.id,
+                    'question[question_text]': overlayHTML
+                }, (err) => {
+                    if (err) {
+                        course.throwErr('quizFixOverlay', `Unable to fix the overlay. Quiz: ${quiz.title} questionId${question.id}`);
+                        replaceCb(null);
+                        return;
+                    }
+                    course.success('quizFixOverlay', `Updated overlay Quiz: ${quiz.title} questionId${question.id}`);
+                    replaceCb(null);
+                });
+            } else {
+                course.success('quizFixOverlay', `CDATA was not found in quiz: ${quiz.title} question: ${question.id}`);
+            }
+            replaceCb(null);
+        }
+
+
+        canvas.get(`https://byui.instructure.com/api/v1/courses/${course.info.canvasOU}/quizzes/${quiz.id}`, (err, questions) => {
+            if (err) {
+                course.throwErr(`quizFixOverlay', 'Could not get quiz Questions from Canvas err: ${err}, quiz: ${quiz.title}`);
+                questionCb(null, course);
+                return;
+            }
+            asyncLib.each(questions, replaceHTML, () => {
+                /* No errs should ever be passed to this point. Otherwise one failed question update will stop all questions from updating */
+                questionCb(null);
+            })
+        });
+    }
+
+
+    /* Get all quizzes in the course */
+    canvas.get(`https://byui.instructure.com/api/v1/courses/${course.info.canvasOU}/quizzes`, (err, quizzes) => {
+        if (err) {
+            course.throwErr(`quizFixOverlay', 'Could not get quizzes from Canvas: ${err}`);
+            stepCallback(null, course);
+            return;
+        }
+        asyncLib.each(quizzes, getQuestions, (err) => {
+            if (err) {
+                // I don't think an err should ever really make it...
+            }
+            stepCallback(null, course);
+        });
+    });
 };
